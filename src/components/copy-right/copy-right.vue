a5286d8207d1f82858f7843af20c9baf012f3339
Josh Masen <josh@getflywheel.com>
2020-12-17 23:15:30 -0700

    Clean up repo and add stylesheet

--- src/components/copy-right/copy-right.vue
+++ src/components/copy-right/copy-right.vue
@@ -1,9 +1,3 @@
 <template>
-  <footer class="info">
-    <p>Double-click to edit a todo</p>
-    <p>
-      Created by <a href="http://github.com/blacksonic/"><x-username /></a>
-    </p>
-    <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
-  </footer>
+  <footer class="info"></footer>
 </template>
